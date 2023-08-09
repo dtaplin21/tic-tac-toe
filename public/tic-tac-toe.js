08d51f337042b08e6838ecd881517333d3b0dfc2
Tim Mairose <tmairose@gmail.com>
2023-08-08 23:04:35 -0600

    most of part1 done

--- public/tic-tac-toe.js
+++ public/tic-tac-toe.js
@@ -1,1 +1,10 @@
-// Your code here
+window.onload = () => {
+    const gameBoard = document.getElementById("game-board");
+    const playerTurn = "x";
+
+    gameBoard.addEventListener("click", (e) => {
+        e.target.classList.add(playerTurn);
+        console.log(e.target);
+        playerTurn = playerTurn === "x" ? "o" : "x";
+    })
+}
