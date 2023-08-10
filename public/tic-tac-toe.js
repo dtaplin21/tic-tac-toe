7e83b4bf621e8beebdd5d0fbeaf36fdff616fde4
Tim Mairose <tmairose@gmail.com>
2023-08-09 23:07:34 -0600

    got alot of stuff done but not all

--- public/tic-tac-toe.js
+++ public/tic-tac-toe.js
@@ -1,41 +1,75 @@
+    const squareZero = document.getElementById("square-0");
+    const squareOne = document.getElementById("square-1");
+    const squareTwo = document.getElementById("square-2");
+    const squareThree = document.getElementById("square-3");
+    const squareFour = document.getElementById("square-4");
+    const squareFive = document.getElementById("square-5");
+    const squareSix = document.getElementById("square-6");
+    const squareSeven = document.getElementById("square-7");
+    const squareEight = document.getElementById("square-8");
+
+    const win =[
+        [squareZero, squareThree, squareSix],
+        [squareOne, squareFour, squareSeven],
+        [squareTwo, squareFive, squareEight],
+        [squareZero, squareFour, squareEight],
+        [squareTwo, squareFour, squareSix],
+        [squareZero, squareOne, squareTwo],
+        [squareThree, squareFour, squareFive],
+        [squareSix, squareSeven, squareEight]
+    ]
+
 let gameStart = true
 window.onload = () => {
     const gameBoard = document.getElementById("game-board");
-    const playerTurn = "x";
+    let playerTurn = "x";
 
     gameBoard.addEventListener("click", (e) => {
-        e.target.classList.add(playerTurn);
+        e.target.innerText = playerTurn;
         console.log(e.target);
+        console.log(e.target.classList);
         playerTurn = playerTurn === "x" ? "o" : "x";
-        const win =
-        [ [0, 3, 6]
-         [1, 4, 7]
-         [2, 5, 8] ]
+
+        //set value or innerText of the clicked Square to the currPlayer (x or o) [DONE]
+        //prevent square with value from being overwritten []
+        //
+
+        
+
+        
 
 
-       bestPlayer()
+        bestPlayer(win)
     });
 
-    const header = document.getElementById("displayWinner");
-       const newHeading = document.createElement("h1");
-       newHeading.innerText = "Winner";
-       header.appendChild(newHeading);
-      winner()
+    // const header = document.getElementById("displayWinner");
+    //     const newHeading = document.createElement("h1");
+    //     newHeading.innerText = "Winner";
+    //     header.appendChild(newHeading);
+    //     winner()
 }
 
 function winner (winnerName) {
-    const newHeading = document.getElementbyId("h1")
-    const displayWinner = document.getElementbyId('displayWinner');
+    // const newHeading = document.getElementById("h1")
+    const displayWinner = document.getElementById('displayWinner');
     displayWinner.innerText = `winner: ${winnerName}`
-    newHeading.style.display = "flex";
-    newHeading.style.position = "default"
+    displayWinner.style.display = "flex";
+    displayWinner.style.position = "default"
 }
 
 
- function bestPlayer (win) {
+function bestPlayer (win) {
+    
     win.forEach(set => {
-        if ((`square-${set[0]}` === "x" && `square-${set[1]}` === "x" && `square-${set[2]}` === "x") ||  (`square-${set[0]}` === "o" && `square-${set[1]}` === "o" && `square-${set[2]}` === "o")) {
+        if ((`square-${set[0]}` === "x" 
+        && `square-${set[1]}` === "x" 
+        && `square-${set[2]}` === "x") 
+        || (`square-${set[0]}` === "o" 
+        && `square-${set[1]}` === "o" 
+        && `square-${set[2]}` === "o")) {
             return winner()
         }
     })
 }
+
+    
