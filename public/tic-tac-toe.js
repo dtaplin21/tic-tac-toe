c492fdefeabe211c6008deaaaa644bce03147eb4
Dom <dtaplin21@aol.com>
2023-08-09 20:23:37 -0700

    tic

--- public/tic-tac-toe.js
+++ public/tic-tac-toe.js
@@ -1,3 +1,4 @@
+let gameStart = true
 window.onload = () => {
     const gameBoard = document.getElementById("game-board");
     const playerTurn = "x";
@@ -6,5 +7,35 @@ window.onload = () => {
         e.target.classList.add(playerTurn);
         console.log(e.target);
         playerTurn = playerTurn === "x" ? "o" : "x";
+        const win =
+        [ [0, 3, 6]
+         [1, 4, 7]
+         [2, 5, 8] ]
+
+
+       bestPlayer()
+    });
+
+    const header = document.getElementById("displayWinner");
+       const newHeading = document.createElement("h1");
+       newHeading.innerText = "Winner";
+       header.appendChild(newHeading);
+      winner()
+}
+
+function winner (winnerName) {
+    const newHeading = document.getElementbyId("h1")
+    const displayWinner = document.getElementbyId('displayWinner');
+    displayWinner.innerText = `winner: ${winnerName}`
+    newHeading.style.display = "flex";
+    newHeading.style.position = "default"
+}
+
+
+ function bestPlayer (win) {
+    win.forEach(set => {
+        if ((`square-${set[0]}` === "x" && `square-${set[1]}` === "x" && `square-${set[2]}` === "x") ||  (`square-${set[0]}` === "o" && `square-${set[1]}` === "o" && `square-${set[2]}` === "o")) {
+            return winner()
+        }
     })
 }
